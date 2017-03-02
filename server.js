d1c9970a19e0d0c71b8506a441e811b69efdef81
mcastre <mcastre3@gmail.com>
2017-03-02 15:38:56 -0600

    Fix typo

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-  if (req.headers['x-forwared-proto'] === 'http') {
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
-    res.redirect('http://' + req.hostname + req.url); 
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
