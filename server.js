0556804055af8ab26918915073ed729a831aa0e5
mcastre <mcastre3@gmail.com>
2017-03-02 15:49:18 -0600

    Server file changes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
+// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
+app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
@@ -13,6 +14,6 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-  console.log('Server up and running on ' + PORT);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
